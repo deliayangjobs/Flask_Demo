d67c3ddf16768d82eff29d5b49078161d56f3913
Delia Yang <deliay@surveymonkey.com>
2017-10-31 17:06:04 -0700

    BACKBONE Model

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,9 @@
+var Song = Backbone.Model.extend({
+    validate: function(attrs){
+        if(!attrs.title){
+            return "Title is required";
+        }
+    }
+});
 
-// In the first few sections, we do all the coding here.
-// Later, you'll see how to organize your code into separate
-// files and modules.
+var song = new Song();
